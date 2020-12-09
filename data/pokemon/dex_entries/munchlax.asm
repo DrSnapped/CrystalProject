07b6656ab98ab6d162a8190b2464fbdf701c8d22
DrSnapped <Bagan13666@hotmail.com>
2020-12-08 22:07:12 -0600

    Create munchlax.asm
    Pasted, renamed to munchlax

---
+++ data/pokemon/dex_entries/munchlax.asm
@@ -0,0 +1,9 @@
+	db "OGRE DARNER@" ; species name
+	dw 603, 1135 ; height, weight
+
+	db   "By churning its"
+	next "wings, it creates"
+	next "shock waves that"
+	page "inflict critical"
+	next "internal injuries"
+	next "on foes.@"
