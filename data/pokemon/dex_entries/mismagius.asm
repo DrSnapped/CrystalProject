2bd371266170ff905896d9a0efde5364d47b0bf6
DrSnapped <Bagan13666@hotmail.com>
2020-12-11 04:30:40 -0600

    Create mismagius.asm
    Created mismagius.asm dex entry file

---
+++ data/pokemon/dex_entries/mismagius.asm
@@ -0,0 +1,9 @@
+	db "MAGICAL@" ; species name
+	dw 211, 97 ; height, weight
+
+	db   "Its cries sound"
+	next "like incantations"
+	next "to torment the"
+	page "foe. It appears"
+	next "where you least"
+	next "expect it.@"
